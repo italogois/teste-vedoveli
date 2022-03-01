86b5385c6671723537b14ba2621228d6314d9685
Italo Gois <italo.alberto26@gmail.com>
2022-03-01 16:31:24 -0300

    query string add parse to object and tests

--- projeto1/src/lib/queryString.js
+++ projeto1/src/lib/queryString.js
@@ -1,5 +1,28 @@
 module.exports.queryString = (obj) => {
-  const arr = Object.entries(obj).map((item) => `${item[0]}=${item[1]}`);
+  const arr = Object.entries(obj)
+    .map(([key, value]) => {
+      if (typeof value === "object" && !Array.isArray(value)) {
+        throw new Error("Invalid params");
+      }
 
-  return arr.join("&");
+      return `${key}=${value}`;
+    })
+    .join("&");
+
+  return arr;
+};
+
+module.exports.parse = (qs) => {
+  const qsSplit = qs.split("&").map((item) => {
+    let [key, value] = item.split("=");
+
+    if (value.includes(",")) {
+      value = value.split(",");
+    }
+
+    return [key, value];
+  });
+  const obj = Object.fromEntries(qsSplit);
+
+  return obj;
 };
--- projeto1/src/lib/queryString.spec.js
+++ projeto1/src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require("./queryString");
+const { queryString, parse } = require("./queryString");
 
 describe("Object to query string", () => {
   it("should be create a query string when object is provide", () => {
@@ -8,5 +8,51 @@ describe("Object to query string", () => {
     };
 
     expect(queryString(obj)).toBe("name=italo&profession=development");
+  });
+
+  it("should be create a query string when array is value", () => {
+    const obj = {
+      name: "italo",
+      skills: ["js", "html"],
+    };
+
+    expect(queryString(obj)).toBe("name=italo&skills=js,html");
+  });
+
+  it("should be throw when in object has object", () => {
+    const obj = {
+      name: "italo",
+      skills: { first: "js", second: "html" },
+    };
+
+    expect(() => queryString(obj)).toThrowError();
+  });
+});
+
+describe("query String to Object", () => {
+  it("should be convet query string to object", () => {
+    const queryString = "name=italo&profession=development";
+
+    expect(parse(queryString)).toMatchObject({
+      name: "italo",
+      profession: "development",
+    });
+  });
+
+  it("should be convet query string to object with one key-value", () => {
+    const queryString = "name=italo";
+
+    expect(parse(queryString)).toMatchObject({
+      name: "italo",
+    });
+  });
+
+  it("should be convet query string to object with comma operator in value", () => {
+    const queryString = "name=italo&skills=js,html";
+
+    expect(parse(queryString)).toMatchObject({
+      name: "italo",
+      skills: ["js", "html"],
+    });
   });
 });
